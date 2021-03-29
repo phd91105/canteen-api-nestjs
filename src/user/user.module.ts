b211461e294c7a551c616e00528bea8518fee179
Pham Duy <phd051199@gmail.com>
2021-03-30 00:12:33 +0700

    Complete JWT Authentication

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,10 +3,20 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './user.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity])],
-  providers: [UserService],
+  imports: [
+    PassportModule,
+    TypeOrmModule.forFeature([UserEntity]),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'secretKey',
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
+  providers: [UserService, JwtStrategy],
   controllers: [UserController],
 })
 export class UserModule {}
