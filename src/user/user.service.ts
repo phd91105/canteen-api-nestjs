4ec7ab87db0024e0c86dde41fe8b3ec665b1961f
Pham Duy <phd051199@gmail.com>
2021-03-29 12:50:19 +0700

    Hasing and decrypt password method

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Body, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { DeleteResult, Repository, UpdateResult } from 'typeorm';
 import { UserEntity } from './user.entity';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
@@ -17,8 +18,14 @@ export class UserService {
     return await this.userRepo.findOne(id);
   }
 
-  async create(user: UserEntity): Promise<UserEntity> {
-    return await this.userRepo.save(user);
+  async register(user: UserEntity): Promise<UserEntity> {
+    const hashedPassword = await bcrypt.hash(user.password, 12);
+    return this.userRepo.save({
+      email: user.email,
+      fullName: user.fullName,
+      username: user.username,
+      password: hashedPassword,
+    });
   }
 
   async update(user: UserEntity): Promise<UpdateResult> {
@@ -27,5 +34,16 @@ export class UserService {
 
   async delete(id: number): Promise<DeleteResult> {
     return await this.userRepo.delete(id);
+  }
+
+  async login(username: string, password: string): Promise<any> {
+    const user = await this.userRepo.findOne({ username });
+    if (!user) {
+      throw new BadRequestException('Invalid username');
+    }
+    if (!(await bcrypt.compare(password, user.password))) {
+      throw new BadRequestException('Invalid password');
+    }
+    return { userInfo: user, token: '' };
   }
 }
