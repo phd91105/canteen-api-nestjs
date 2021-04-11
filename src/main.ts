4c00d3634d845336d01d1f00e27a297847f11683
Pham Duy <phd051199@gmail.com>
2021-04-11 10:49:40 +0700

    Change npm > yarn

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
