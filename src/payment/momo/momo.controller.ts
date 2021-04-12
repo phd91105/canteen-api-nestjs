9c2731497684a649db479258f50534a50eaf1ce0
Pham Duy <phd051199@gmail.com>
2021-04-12 08:44:48 +0700

    Swagger for momo service

--- src/payment/momo/momo.controller.ts
+++ src/payment/momo/momo.controller.ts
@@ -1,10 +1,20 @@
 import { Body, Controller, Get, Post, Query } from '@nestjs/common';
+import { ApiBody, ApiProperty } from '@nestjs/swagger';
 import { MomoService } from './momo.service';
+
+class MomoBody {
+  @ApiProperty({ type: Number })
+  amount: number;
+
+  @ApiProperty({ type: String })
+  msg: string;
+}
 
 @Controller('momo')
 export class MomoController {
   constructor(private readonly momoService: MomoService) {}
 
+  @ApiBody({ type: MomoBody })
   @Post('payment')
   payment(@Body('amount') _amount?: number, @Body('msg') msg?: string) {
     return this.momoService.payment(_amount, msg);
